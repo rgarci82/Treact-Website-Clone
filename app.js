0b3f7c0f821dbf940a8b65f8697e4cb6d6d36691
rgarci82 <ruben.garcia30102@gmail.com>
2023-10-24 23:23:56 -0500

    moved javascript

--- app.js
+++
@@ -1,8 +0,0 @@
-function openMenu(){
-    if(!document.body.classList.contains("menu--open")){
-        document.body.classList += " menu--open"
-    }
-    else{
-        document.body.classList -= " menu--open"
-    }
-}
